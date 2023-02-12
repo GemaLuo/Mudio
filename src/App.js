174bf8379a5fcc277e4c6147b6d8a9a1a915369a
GemaLuo <gemaluowenjun@gmail.com>
2023-02-12 15:45:55 +0800

    [setup] webpack & tailwind & react & postcss

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function App() {
+  return <h1 class="text-center text-3xl font-bold mx-auto bg-slate-500 underline">
+  Hello world!
+</h1>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './dist/index.html'
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: {importLoaders: 1 }
+          },
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ],
+  },
+};
